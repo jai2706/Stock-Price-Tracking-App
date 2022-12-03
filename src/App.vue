cf02a666aae92fd1cc8486faceda16976dc184b1
jai2706 <jaivaidya27@gmail.com>
2022-12-03 21:58:35 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- bold and enlarge the text -->
+  <HelloWorld msg="Stock Price Chart" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
